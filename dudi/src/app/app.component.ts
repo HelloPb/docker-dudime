25e4a3d16083e3fec28bec83cfd3df4f7ab5dc25
Prasanna Bhat <prasanna.k.bhat@gmail.com>
2017-06-14 22:41:10 +1000

    hh

--- dudi/src/app/app.component.ts
+++ dudi/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app frm docker google';
+  title = 'app frm docker google.  rebuild';
 }
